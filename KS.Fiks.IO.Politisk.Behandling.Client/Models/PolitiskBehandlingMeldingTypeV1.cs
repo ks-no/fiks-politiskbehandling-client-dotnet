938e0bd7bd0334b62c107c9a19c12a803d068f73
jarle børsheim <jarle.borsheim@gmail.com>
2021-10-18 10:57:32 +0200

    Korrigert meldinstype navn i henhold til schema navn

--- KS.Fiks.IO.Politisk.Behandling.Client/Models/PolitiskBehandlingMeldingTypeV1.cs
+++ KS.Fiks.IO.Politisk.Behandling.Client/Models/PolitiskBehandlingMeldingTypeV1.cs
@@ -3,20 +3,22 @@ namespace KS.Fiks.IO.Politiskbehandling.Client.Models
     public class PolitiskBehandlingMeldingTypeV1
     {
         // Forespørsler
-        public const string HentMoeteplan = "no.ks.fiks.politisk.behandling.klient.hentmoeteplan.v1";
-        public const string HentUtvalg = "no.ks.fiks.politisk.behandling.klient.hentutvalg.v1";
+        public const string HentMoeteplan = "no.ks.fiks.politisk.behandling.hentmoeteplan.v1";
+        public const string HentUtvalg = "no.ks.fiks.politisk.behandling.hentutvalg.v1";
         
-        // Resultat
-        public const string ResultatMoeteplan = "no.ks.fiks.politisk.behandling.tjener.resultatmoeteplan.v1";
-        public const string ResultatUtvalg = "no.ks.fiks.politisk.behandling.tjener.resultatutvalg.v1";
-        public const string ResultatSendVedtakFraUtvalg = "no.ks.fiks.politisk.behandling.tjener.sendvedtakfrautvalg.v1";
+        // Resultat på forespørsler
+        public const string ResultatMoeteplan = "no.ks.fiks.politisk.behandling.resultatmoeteplan.v1";
+        public const string ResultatUtvalg = "no.ks.fiks.politisk.behandling.resultatutvalg.v1";
         public const string Mottatt = "no.ks.fiks.politisk.behandling.mottatt.v1";
-        
+
+        // Utsendelser 
+        public const string SendVedtakFraUtvalg = "no.ks.fiks.politisk.behandling.sendvedtakfrautvalg.v1";
+
         // Innsendelser
-        public const string SendUtvalgssak = "no.ks.fiks.politisk.behandling.klient.sendutvalgssak.v1";
-        public const string SendOrienteringssak = "no.ks.fiks.politisk.behandling.klient.sendorienteringssak.v1";
-        public const string SendDelegertVedtak = "no.ks.fiks.politisk.behandling.klient.senddelegertvedtak.v1";
-        
+        public const string SendUtvalgssak = "no.ks.fiks.politisk.behandling.sendutvalgssak.v1";
+        public const string SendOrienteringssak = "no.ks.fiks.politisk.behandling.sendorienteringssak.v1";
+        public const string SendDelegertVedtak = "no.ks.fiks.politisk.behandling.senddelegertvedtak.v1";
+
         // eInnsyn
         public const string SendMoeteplanTilEInnsyn = "no.ks.fiks.politisk.behandling.eInnsyn.sendmoeteplan.v1";
         public const string SendUtvalgssakerTilEInnsyn = "no.ks.fiks.politisk.behandling.eInnsyn.sendutvalgssaker.v1";
