f27fd12ac55fbe78f6a8ea413f05bbbf53322574
Jarle Børsheim <jarle.borsheim@gmail.com>
2021-10-18 13:15:10 +0200

    #17 Nye meldingstyper for kvittering (#5)
    * Utkast til nye resultat schemas
    
    * Lagt til registrertDato
    
    * Slettet unødvendige schemas og lagt til som meldingstyper i stedet
    
    * Tatt bort mottatt som type

--- KS.Fiks.IO.Politisk.Behandling.Client/Models/PolitiskBehandlingMeldingTypeV1.cs
+++ KS.Fiks.IO.Politisk.Behandling.Client/Models/PolitiskBehandlingMeldingTypeV1.cs
@@ -10,12 +10,15 @@ namespace KS.Fiks.IO.Politiskbehandling.Client.Models
         public const string ResultatMoeteplan = "no.ks.fiks.politisk.behandling.tjener.resultatmoeteplan.v1";
         public const string ResultatUtvalg = "no.ks.fiks.politisk.behandling.tjener.resultatutvalg.v1";
         public const string ResultatSendVedtakFraUtvalg = "no.ks.fiks.politisk.behandling.tjener.sendvedtakfrautvalg.v1";
-        public const string Mottatt = "no.ks.fiks.politisk.behandling.mottatt.v1";
+        public const string ResultatSendVedtakFraUtvalgKvittering = "no.ks.fiks.politisk.behandling.tjener.sendvedtakfrautvalg.kvittering.v1";
         
         // Innsendelser
         public const string SendUtvalgssak = "no.ks.fiks.politisk.behandling.klient.sendutvalgssak.v1";
+        public const string SendUtvalgssakKvittering = "no.ks.fiks.politisk.behandling.klient.sendutvalgssak.kvittering.v1";
         public const string SendOrienteringssak = "no.ks.fiks.politisk.behandling.klient.sendorienteringssak.v1";
+        public const string SendOrienteringssakKvittering = "no.ks.fiks.politisk.behandling.klient.sendorienteringssak.kvittering.v1";
         public const string SendDelegertVedtak = "no.ks.fiks.politisk.behandling.klient.senddelegertvedtak.v1";
+        public const string SendDelegertVedtakKvittering = "no.ks.fiks.politisk.behandling.klient.senddelegertvedtak.kvittering.v1";
         
         // eInnsyn
         public const string SendMoeteplanTilEInnsyn = "no.ks.fiks.politisk.behandling.eInnsyn.sendmoeteplan.v1";
