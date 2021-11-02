837c70615fadb750bf8e87849ecf4277d22b99d6
Jarle Børsheim <jarle.borsheim@gmail.com>
2021-11-02 13:22:12 +0100

    Forslag til bedre navn på meldingstyper (#11)
    * Forslag til bedre navn på meldingstyper
    
    * Korrigert constants også
    
    * Fikset mangler etter merge

--- KS.Fiks.IO.Politisk.Behandling.Client/Models/PolitiskBehandlingMeldingTypeV1.cs
+++ KS.Fiks.IO.Politisk.Behandling.Client/Models/PolitiskBehandlingMeldingTypeV1.cs
@@ -3,28 +3,31 @@ namespace KS.Fiks.IO.Politiskbehandling.Client.Models
     public class PolitiskBehandlingMeldingTypeV1
     {
         // Forespørsler
-        public const string HentMoeteplan = "no.ks.fiks.politisk.behandling.hentmoeteplan.v1";
-        public const string HentUtvalg = "no.ks.fiks.politisk.behandling.hentutvalg.v1";
+        public const string HentMoeteplan = "no.ks.fiks.politisk.behandling.v1.moeteplan.hent";
+        public const string HentUtvalg = "no.ks.fiks.politisk.behandling.v1.utvalg.hent";
         
         // Resultat på forespørsler
-        public const string ResultatMoeteplan = "no.ks.fiks.politisk.behandling.resultatmoeteplan.v1";
-        public const string ResultatUtvalg = "no.ks.fiks.politisk.behandling.resultatutvalg.v1";
+        public const string ResultatMoeteplan = "no.ks.fiks.politisk.behandling.v1.moeteplan.hent.resultat";
+        public const string ResultatUtvalg = "no.ks.fiks.politisk.behandling.v1.utvalg.hent.resultat";
 
         // Utsendelser 
-        public const string SendVedtakFraUtvalg = "no.ks.fiks.politisk.behandling.sendvedtakfrautvalg.v1";
-        public const string SendVedtakFraUtvalgKvittering = "no.ks.fiks.politisk.behandling.sendvedtakfrautvalg.kvittering.v1";
+        public const string SendVedtakFraUtvalg = "no.ks.fiks.politisk.behandling.v1.vedtakfrautvalg.send";
+        public const string SendVedtakFraUtvalgKvittering = "no.ks.fiks.politisk.behandling.v1.vedtakfrautvalg.send.kvittering";
 
         // Innsendelser
-        public const string SendUtvalgssak = "no.ks.fiks.politisk.behandling.sendutvalgssak.v1";
-        public const string SendUtvalgssakKvittering = "no.ks.fiks.politisk.behandling.sendutvalgssak.kvittering.v1";
-        public const string SendOrienteringssak = "no.ks.fiks.politisk.behandling.sendorienteringssak.v1";
-        public const string SendOrienteringssakKvittering = "no.ks.fiks.politisk.behandling.sendorienteringssak.kvittering.v1";
-        public const string SendDelegertVedtak = "no.ks.fiks.politisk.behandling.senddelegertvedtak.v1";
-        public const string SendDelegertVedtakKvittering = "no.ks.fiks.politisk.behandling.senddelegertvedtak.kvittering.v1";
+        public const string SendUtvalgssak = "no.ks.fiks.politisk.behandling.v1.utvalgssak.send";
+        public const string SendUtvalgssakKvittering = "no.ks.fiks.politisk.behandling.v1.utvalgssak.send.kvittering";
+        public const string SendOrienteringssak = "no.ks.fiks.politisk.behandling.v1.orienteringssak.send";
+        public const string SendOrienteringssakKvittering = "no.ks.fiks.politisk.behandling.v1.orienteringssak.send.kvittering";
+        public const string SendDelegertVedtak = "no.ks.fiks.politisk.behandling.v1.delegertvedtak.send";
+        public const string SendDelegertVedtakKvittering = "no.ks.fiks.politisk.behandling.v1.delegertvedtak.send.kvittering";
 
         // eInnsyn
-        public const string SendMoeteplanTilEInnsyn = "no.ks.fiks.politisk.behandling.eInnsyn.sendmoeteplan.v1"; //TODO er dette eller schema navn korrekt?
-        public const string SendUtvalgssakerTilEInnsyn = "no.ks.fiks.politisk.behandling.eInnsyn.sendutvalgssaker.v1"; //TODO er dette eller schema navn korrekt?
-        public const string SendVedtakTilEInnsyn = "no.ks.fiks.politisk.behandling.eInnsyn.sendvedtak.v1"; //TODO er dette eller schema navn korrekt?
+        public const string SendMoeteplanTilEInnsyn = "no.ks.fiks.politisk.behandling.v1.eInnsyn.moeteplan.send";
+        public const string SendMoeteplanTilEInnsynKvittering = "no.ks.fiks.politisk.behandling.v1.eInnsyn.moeteplan.send.kvittering";
+        public const string SendUtvalgssakerTilEInnsyn = "no.ks.fiks.politisk.behandling.v1.eInnsyn.utvalgssaker.send";
+        public const string SendUtvalgssakerTilEInnsynKvittering = "no.ks.fiks.politisk.behandling.v1.eInnsyn.utvalgssaker.send.kvittering";
+        public const string SendVedtakTilEInnsyn = "no.ks.fiks.politisk.behandling.v1.eInnsyn.vedtak.send";
+        public const string SendVedtakTilEInnsynKvittering = "no.ks.fiks.politisk.behandling.v1.eInnsyn.vedtak.send.kvittering";
     }
 }
